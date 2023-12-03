95ba82f67949f3fdf3e0f3f2772703e9071f2fad
Rubina Noor <rubinanoor29@gmail.com>
2023-12-03 22:38:26 +0500

    SignUp UI edited

--- src/View/SignUp.java
+++ src/View/SignUp.java
@@ -17,70 +17,88 @@ public class SignUp extends JFrame implements ActionListener {
     private JPasswordField pass;
     private JPasswordField cPass;
     private JButton signIn;
+    private JButton backToLogin; // Added button to go back to login page
     private static Connection connection;
 
     //---------------------------------------------------------------------------------------------------------
     public void createSignUpPage(final Connection conn) {
-             this.connection = conn;
-            l1 = new JLabel("Create Username: ");
-            l2 = new JLabel("Create Password: ");
-            l3 = new JLabel("Confirm Password: ");
-            uName = new JTextField(20);
-            pass = new JPasswordField(20);
-            cPass = new JPasswordField(20);
-            signIn = new JButton("SignUp");
-            signIn.addActionListener(this);
+        this.connection = conn;
+        l1 = new JLabel("Create Username: ");
+        l2 = new JLabel("Create Password: ");
+        l3 = new JLabel("Confirm Password: ");
+        uName = new JTextField(20);
+        pass = new JPasswordField(20);
+        cPass = new JPasswordField(20);
+        signIn = new JButton("SignUp");
+        signIn.addActionListener(this);
+        signIn.setBackground(new Color(39, 66, 122));
 
-            // Set up the left panel with a GridBagLayout
-            p1 = new JPanel(new GridBagLayout());
-            GridBagConstraints gbc = new GridBagConstraints();
-            gbc.gridwidth = GridBagConstraints.REMAINDER;
-            gbc.insets = new Insets(0, 0, 20, 0); // Add spacing between components
+        // Added backToLogin button
+        backToLogin = new JButton("Login");
+        backToLogin.addActionListener(this);
+        backToLogin.setBackground(new Color(39, 66, 122));
+        backToLogin.setForeground(Color.WHITE);
+        signIn.setForeground(Color.WHITE);
 
-            p1.add(l1, gbc);
-            p1.add(uName, gbc);
-            p1.add(l2, gbc);
-            p1.add(pass, gbc);
-            p1.add(l3, gbc);
-            p1.add(cPass, gbc);
-          p1.add(signIn,gbc);
-            // Set background color for the entire content pane
-            p1.setBackground(new Color(255, 240, 245)); // Lavender Blush
+        p1 = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.insets = new Insets(0, 0, 20, 0); // Add spacing between components
 
-            // Create a right panel
-            JPanel rightPanel = new JPanel(new BorderLayout());
-            rightPanel.setBackground(Color.WHITE);
-            JLabel smartScriptLabel = new JLabel("SmartScript");
-            smartScriptLabel.setHorizontalAlignment(JLabel.CENTER);
-            smartScriptLabel.setFont(new Font("Arial", Font.BOLD, 20));
-            smartScriptLabel.setForeground(Color.WHITE);
-            JPanel smartScriptPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 180));
-            smartScriptPanel.setBackground(new Color(70, 130, 180));
-            smartScriptPanel.add(smartScriptLabel);
-            rightPanel.add(smartScriptPanel, BorderLayout.CENTER);
+        p1.add(l1, gbc);
+        p1.add(uName, gbc);
+        p1.add(l2, gbc);
+        p1.add(pass, gbc);
+        p1.add(l3, gbc);
+        p1.add(cPass, gbc);
+        p1.add(signIn, gbc);
+        p1.add(backToLogin, gbc); // Added backToLogin button
 
-            // Create a JSplitPane
-            JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, p1, rightPanel);
-            splitPane.setDividerLocation(200);
+        // Set background color for the entire content pane
+        p1.setBackground(new Color(255, 240, 245)); // Lavender Blush
 
-            // Frame
-            setLayout(new GridLayout(1, 1));
-            add(splitPane);
-            setTitle("SignUp Page");
-            setSize(500, 600);
-            addWindowListener(new MyWindowListener());
-            setLocationRelativeTo(null);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setVisible(true);
-        }
+        // Create a right panel
+        JPanel rightPanel = new JPanel(new BorderLayout());
+        rightPanel.setBackground(Color.WHITE);
+        JLabel smartScriptLabel = new JLabel("SmartScript");
+        smartScriptLabel.setHorizontalAlignment(JLabel.CENTER);
+        smartScriptLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        smartScriptLabel.setForeground(Color.WHITE);
+        JPanel smartScriptPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 180));
+        smartScriptPanel.setBackground(new Color(39, 66, 122));
+        smartScriptPanel.add(smartScriptLabel);
+        rightPanel.add(smartScriptPanel, BorderLayout.CENTER);
 
-        //---------------------------------------------------------------------------------------------------------
+        // Create a JSplitPane
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, p1, rightPanel);
+        splitPane.setDividerLocation(300);
+
+        // Frame
+        setLayout(new GridLayout(1, 1));
+        add(splitPane);
+        setTitle("SignUp Page");
+        setSize(600, 600);
+        addWindowListener(new MyWindowListener());
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    //---------------------------------------------------------------------------------------------------------
     @Override
     public void actionPerformed(ActionEvent e) {
-        try {
-            saveData();
-        } catch (SQLException ex) {
-            throw new RuntimeException(ex);
+        if (e.getActionCommand().equals("SignUp")) {
+            try {
+                saveData();
+                Login loginPage = new Login();
+                loginPage.createLoginPage(connection);
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        } else if (e.getActionCommand().equals("Login")) {
+            dispose();
+            Login loginPage = new Login();
+            loginPage.createLoginPage(connection);
         }
     }
 
@@ -124,6 +142,10 @@ public class SignUp extends JFrame implements ActionListener {
         if (!resultSet.next()) {
             String st1 = new String(pass.getPassword());
             String st2 = new String(cPass.getPassword());
+            if (uName.getText().equals("") || st1.equals("") || st2.equals("")) {
+                JOptionPane.showMessageDialog(this, "Fields empty!");
+                return;
+            }
             if (st1.equals(st2)) {
                 query = "insert into users values (?, ?)";
                 try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
